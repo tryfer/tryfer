51125d59f4654a1d884a90cae58deab628c9974e
David <dreid@dreid.org>
2012-08-07 15:56:44 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+def refresh_plugin_cache():
+    from twisted.plugin import IPlugin, getPlugins
+    list(getPlugins(IPlugin))
+
+setup(name='tryfer',
+      version='0.1',
+      description='Twisted Zipkin Tracing Library',
+      packages=['tryfer'])
+
+refresh_plugin_cache()
