aeb7b968be5af60a705048aa9d5bcd05214e9dd4
David <dreid@dreid.org>
2012-08-15 19:11:50 -0700

    better setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
+
 from twisted.python.dist import getPackages
 
-
-def refresh_plugin_cache():
-    from twisted.plugin import IPlugin, getPlugins
-    list(getPlugins(IPlugin))
-
-setup(name='tryfer',
-      version='0.1',
-      description='Twisted Zipkin Tracing Library',
-      packages=getPackages('tryfer'))
-
-refresh_plugin_cache()
+setup(
+    name='tryfer',
+    version='0.1',
+    description='Twisted Zipkin Tracing Library',
+    packages=getPackages('tryfer'),
+    install_requires=['Twisted', 'thrift', 'scrivener'],
+)
