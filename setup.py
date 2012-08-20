e417ef115b6973e65715cb3d2ae8238285569246
David <dreid@dreid.org>
2012-08-20 15:21:56 -0700

    Apache Licensed w/ appropriate copyright headers and trove classifiers.

--- setup.py
+++ setup.py
@@ -6,6 +6,15 @@ setup(
     name='tryfer',
     version='0.1',
     description='Twisted Zipkin Tracing Library',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Twisted'
+    ],
+    license='APL2',
+    url='https://github.com/racker/tryfer',
+
     packages=getPackages('tryfer'),
     install_requires=['Twisted', 'thrift', 'scrivener'],
 )
--- tryfer/keystone.py
+++ tryfer/keystone.py
@@ -1,11 +1,23 @@
+# Copyright 2012 Rackspace Hosting, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
 from cStringIO import StringIO
 from Queue import Queue
-from twisted.internet import reactor
 from twisted.internet.defer import Deferred, succeed, fail
 from twisted.internet.protocol import Protocol
-from twisted.internet.error import ConnectionLost, ConnectionDone, ConnectError
 from twisted.web.client import FileBodyProducer
 from twisted.web.http_headers import Headers
 from twisted.python import log
