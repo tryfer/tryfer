af961f9f9a9c42747f02d7ebe1bc5f6ad1ffdaf2
David <dreid@dreid.org>
2012-08-20 10:32:58 -0700

    Comments on headers and annotation names

--- tryfer/http.py
+++ tryfer/http.py
@@ -17,12 +17,20 @@ class TracingAgent(object):
         if headers is None:
             headers = Headers({})
 
+        # These headers are based on the headers used by finagle's tracing
+        # http Codec.  https://github.com/twitter/finagle/blob/master/finagle-http/src/main/scala/com/twitter/finagle/http/Codec.scala#L200
+        #
+        # Currently not implemented are X-B3-Sampled and X-B3-Flags
+        # Tryfer's underlying Trace implementation has no notion of a Sampled
+        # trace and I haven't figured out what flags are for.
         headers.addRawHeader('X-B3-TraceId', trace.trace_id)
         headers.addRawHeader('X-B3-SpanId', trace.span_id)
 
         if trace.parent_span_id is not None:
             headers.addRawHeader('X-B3-ParentSpanId', trace.parent_span_id)
 
+        # Similar to the headers above we use the annotation 'http.uri' for
+        # because that is the standard set forth in the finagle http Codec.
         trace.record('http.uri', Annotation.string(url))
         trace.record(Annotation.client_send())
 
