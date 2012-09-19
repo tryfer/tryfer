fc7fe2ccc16b0c631503a5ba4267ed68e3595903
David Reid <dreid@dreid.org>
2012-09-19 11:06:02 -0700

    Add a log abstraction that allows you to globally toggle debug logging in tryfer.

---
+++ tryfer/log.py
@@ -0,0 +1,22 @@
+from twisted.python import log
+
+_debugging = False
+
+def set_debugging(on=True):
+    global _debugging
+    _debugging = on
+
+def debug(*args, **kwargs):
+    if _debugging:
+        kwargs['logLevel'] = 'DEBUG'
+        if 'format' in kwargs:
+            kwargs['format'] = '%(logLevel)s: ' + kwargs['format']
+
+        else:
+            args[0] = '%s: %s' % (kwargs['logLevel'], args[0])
+
+        log.msg(*args, **kwargs)
+
+
+msg = log.msg
+err = log.err
