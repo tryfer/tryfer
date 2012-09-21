89dc47207a1821a6d61b50f6efdb4e8d38da0e59
David Reid <dreid@dreid.org>
2012-09-21 11:09:17 -0700

    0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tryfer',
-    version='0.2.0',
+    version='0.2.1',
     description='Twisted Zipkin Tracing Library',
     classifiers=[
         'Development Status :: 4 - Beta',
