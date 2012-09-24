665637ec331601f0a936a817d4ab5e7e228caa7f
David Reid <dreid@dreid.org>
2012-09-24 09:48:43 -0700

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tryfer',
-    version='0.2.1',
+    version='0.2.2',
     description='Twisted Zipkin Tracing Library',
     classifiers=[
         'Development Status :: 4 - Beta',
